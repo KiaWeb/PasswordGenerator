d4dbda6002d9ae89f85e19d49e424767abf7c67a
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-02-02 10:20:14 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import time
+import os
+f=open("keys.txt","a")
+def run():
+    a=open("use.txt","r")
+    a=a.readlines()[1]
+    b=random.choice(a)+random.choice(a)+random.choice(a)
+    f.write("\n"+b)
+    print("Appended "+b+" to keys")
+def start2(tmes):
+    tames = 0
+    elapsed = 0
+    while True:
+        while True:
+            elapsed = elapsed + 1
+            time.sleep(1)
+            if tames == tmes:
+                break
+        run()
+        tames = tames + 1
+        if tames == tmes:
+            f.close()
+            print("Stopped, check keys.txt. Password amount: "+str(tames)+".")
+            time.sleep(3)
+            break
+def start():
+    tmes = input("How many passwords would you like this to generate? e.g 3: ")
+    tmes = int(tmes)
+    start2(tmes)
+start()
